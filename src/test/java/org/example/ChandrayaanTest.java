d541dc2e5766f3e7d7172c573ccf6c3e41521abb
Pranav Bansod <pranav.bansod.14@gmail.com>
2024-01-30 11:07:40 +0530

    WIP: test input (need to handle up and down command)

--- src/test/java/org/example/ChandrayaanTest.java
+++ src/test/java/org/example/ChandrayaanTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.direction.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -497,6 +498,25 @@ class ChandrayaanTest {
 
             DirectionAbstract direction = chandrayaan.getDirection();
             Assertions.assertEquals(NorthDirection.class, direction.getClass());
+        }
+    }
+
+    @Nested
+    class CombinedCommandTest {
+        @Test
+        @Disabled("ToDo: need to handle up and down command in change direction")
+        void testInput1() {
+            CoOrdinates coOrdinates = new CoOrdinates(0, 0, 0);
+            Chandrayaan chandrayaan = new Chandrayaan(coOrdinates, NorthDirection.createInstance());
+            List<String> commands = List.of("f", "r", "u", "b", "l");
+
+            chandrayaan.executeCommands(commands);
+
+            DirectionAbstract directionAbstract = chandrayaan.getDirection();
+            Assertions.assertEquals(NorthDirection.class, directionAbstract.getClass());
+
+            CoOrdinates expectedCoOrdinates = new CoOrdinates(0, 1, -1);
+            Assertions.assertEquals(expectedCoOrdinates, chandrayaan.getCoOrdinates());
         }
     }
 }
