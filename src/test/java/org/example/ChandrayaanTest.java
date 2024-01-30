72beaffc45b10e94d087c46f46c4c2ac7e8fda55
Pranav Bansod <pranav.bansod.14@gmail.com>
2024-01-30 07:19:48 +0530

    Create Nested test class for move and change direction

--- src/test/java/org/example/ChandrayaanTest.java
+++ src/test/java/org/example/ChandrayaanTest.java
@@ -1,229 +1,237 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class ChandrayaanTest {
 
-    @Test
-    void shouldMoveYPosTo1ForFCommandFacingNorth() {
+    @Nested
+    class MoveForwardTest {
+        @Test
+        void shouldMoveYPosTo1ForFCommandFacingNorth() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
+            List<String> commands = List.of("f");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(1, chandrayaan.getYPosition());
+            Assertions.assertEquals(1, chandrayaan.getYPosition());
+        }
+
+        @Test
+        void shouldMoveYPosFrom1To2ForFCommandFacingNorth() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 1, 0, "N");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(2, chandrayaan.getYPosition());
+        }
+
+        @Test
+        void shouldMoveYPosTo1ForFCommandFacingEast() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(1, chandrayaan.getXPosition());
+        }
+
+        @Test
+        void shouldMoveXPosFrom1To2ForFCommandFacingEast() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(1, 0, 0, "E");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(2, chandrayaan.getXPosition());
+        }
+
+        @Test
+        void shouldMoveYPos0ToNeg1ForFCommandFacingWest() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-1, chandrayaan.getXPosition());
+        }
+
+        @Test
+        void shouldMoveXPosFromNeg1ToNeg2ForFCommandFacingWest() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(-1, 0, 0, "W");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-2, chandrayaan.getXPosition());
+        }
+
+        @Test
+        void shouldMoveYPosFrom0ToNeg1ForFCommandFacingSouth() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-1, chandrayaan.getYPosition());
+        }
+
+        @Test
+        void shouldMoveYPosFromNeg1ToNeg2ForFCommandFacingSouth() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, -1, 0, "S");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-2, chandrayaan.getYPosition());
+        }
+
+        @Test
+        void shouldMoveZPosFrom0To1ForFCommandFacingUp() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "U");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(1, chandrayaan.getZPosition());
+        }
+
+        @Test
+        void shouldMoveZPosFrom1To2ForFCommandFacingUp() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 1, "U");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(2, chandrayaan.getZPosition());
+        }
+
+        @Test
+        void shouldMoveZPosFrom0ToNeg1ForFCommandFacingDown() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "D");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-1, chandrayaan.getZPosition());
+        }
+
+        @Test
+        void shouldMoveZPosFromNeg1ToNeg2ForFCommandFacingDown() {
+
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, -1, "D");
+            List<String> commands = List.of("f");
+
+            chandrayaan.executeCommands(commands);
+
+            Assertions.assertEquals(-2, chandrayaan.getZPosition());
+        }
+
     }
 
-    @Test
-    void shouldMoveYPosFrom1To2ForFCommandFacingNorth() {
+    @Nested
+    class ChangeDirectionTest {
+        @Test
+        void shouldChangeDirectionFromNorthToEastOnRightCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 1, 0, "N");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
+            List<String> commands = List.of("r");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(2, chandrayaan.getYPosition());
-    }
+            Assertions.assertEquals("E", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveYPosTo1ForFCommandFacingEast() {
+        @Test
+        void shouldChangeDirectionFromNorthToWestOnLeftCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
+            List<String> commands = List.of("l");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(1, chandrayaan.getXPosition());
-    }
+            Assertions.assertEquals("W", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveXPosFrom1To2ForFCommandFacingEast() {
+        @Test
+        void shouldChangeDirectionFromEastToSouthOnRightCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(1, 0, 0, "E");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
+            List<String> commands = List.of("r");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(2, chandrayaan.getXPosition());
-    }
+            Assertions.assertEquals("S", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveYPos0ToNeg1ForFCommandFacingWest() {
+        @Test
+        void shouldChangeDirectionFromEastToNorthOnLeftCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
+            List<String> commands = List.of("l");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(-1, chandrayaan.getXPosition());
-    }
+            Assertions.assertEquals("N", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveXPosFromNeg1ToNeg2ForFCommandFacingWest() {
+        @Test
+        void shouldChangeDirectionFromSouthToWestOnRightCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(-1, 0, 0, "W");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
+            List<String> commands = List.of("r");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(-2, chandrayaan.getXPosition());
-    }
+            Assertions.assertEquals("W", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveYPosFrom0ToNeg1ForFCommandFacingSouth() {
+        @Test
+        void shouldChangeDirectionFromSouthToEastOnLeftCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
+            List<String> commands = List.of("l");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(-1, chandrayaan.getYPosition());
-    }
+            Assertions.assertEquals("E", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveYPosFromNeg1ToNeg2ForFCommandFacingSouth() {
+        @Test
+        void shouldChangeDirectionFromWestToNorthOnRightCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, -1, 0, "S");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
+            List<String> commands = List.of("r");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(-2, chandrayaan.getYPosition());
-    }
+            Assertions.assertEquals("N", chandrayaan.getDirection());
+        }
 
-    @Test
-    void shouldMoveZPosFrom0To1ForFCommandFacingUp() {
+        @Test
+        void shouldChangeDirectionFromWestToSouthOnLeftCommand() {
 
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "U");
-        List<String> commands = List.of("f");
+            Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
+            List<String> commands = List.of("l");
 
-        chandrayaan.executeCommands(commands);
+            chandrayaan.executeCommands(commands);
 
-        Assertions.assertEquals(1, chandrayaan.getZPosition());
-    }
-
-    @Test
-    void shouldMoveZPosFrom1To2ForFCommandFacingUp() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 1, "U");
-        List<String> commands = List.of("f");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals(2, chandrayaan.getZPosition());
-    }
-
-    @Test
-    void shouldMoveZPosFrom0ToNeg1ForFCommandFacingDown() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "D");
-        List<String> commands = List.of("f");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals(-1, chandrayaan.getZPosition());
-    }
-
-    @Test
-    void shouldMoveZPosFromNeg1ToNeg2ForFCommandFacingDown() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, -1, "D");
-        List<String> commands = List.of("f");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals(-2, chandrayaan.getZPosition());
-    }
-
-    @Test
-    void shouldChangeDirectionFromNorthToEastOnRightCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
-        List<String> commands = List.of("r");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("E", chandrayaan.getDirection());
-    }
-    
-    @Test
-    void shouldChangeDirectionFromNorthToWestOnLeftCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "N");
-        List<String> commands = List.of("l");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("W", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromEastToSouthOnRightCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
-        List<String> commands = List.of("r");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("S", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromEastToNorthOnLeftCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "E");
-        List<String> commands = List.of("l");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("N", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromSouthToWestOnRightCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
-        List<String> commands = List.of("r");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("W", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromSouthToEastOnLeftCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "S");
-        List<String> commands = List.of("l");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("E", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromWestToNorthOnRightCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
-        List<String> commands = List.of("r");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("N", chandrayaan.getDirection());
-    }
-
-    @Test
-    void shouldChangeDirectionFromWestToSouthOnLeftCommand() {
-
-        Chandrayaan chandrayaan = new Chandrayaan(0, 0, 0, "W");
-        List<String> commands = List.of("l");
-
-        chandrayaan.executeCommands(commands);
-
-        Assertions.assertEquals("S", chandrayaan.getDirection());
+            Assertions.assertEquals("S", chandrayaan.getDirection());
+        }
     }
 }
