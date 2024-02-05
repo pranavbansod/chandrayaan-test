da18b4c7a721330ebf3b32446a1e961a5030664d
Pranav Bansod <pranav.bansod.14@gmail.com>
2024-02-05 21:41:34 +0530

    Extract isRightCommand method in Direction enum

--- src/main/java/org/example/Direction.java
+++ src/main/java/org/example/Direction.java
@@ -3,46 +3,6 @@ package org.example;
 public enum Direction {
     E, W, N, S;
 
-    Direction changeDirection(String command) {
-        if (N.equals(this)) {
-            return handleFaceNorth(command);
-        } else if (E.equals(this)) {
-            return handleFaceEast(command);
-        } else if (S.equals(this)) {
-            return handleFaceSouth(command);
-        } else {
-            return handleFaceWest(command);
-        }
-    }
-
-    private static Direction handleFaceSouth(String command) {
-        if ("R".equalsIgnoreCase(command))
-            return W;
-        else
-            return E;
-    }
-
-    private Direction handleFaceEast(String command) {
-        if ("R".equalsIgnoreCase(command))
-            return S;
-        else
-            return N;
-    }
-
-    private Direction handleFaceNorth(String command) {
-        if ("R".equalsIgnoreCase(command))
-            return E;
-        else
-            return W;
-    }
-
-    private Direction handleFaceWest(String command) {
-        if ("R".equalsIgnoreCase(command))
-            return N;
-        else
-            return S;
-    }
-
     void moveForward(CoOrdinates coOrdinates) {
         if (N.equals(this))
             coOrdinates.incrementY();
@@ -63,6 +23,50 @@ public enum Direction {
             coOrdinates.incrementX();
         else if (S.equals(this))
             coOrdinates.incrementY();
+    }
+
+    Direction changeDirection(String command) {
+        if (N.equals(this)) {
+            return handleFaceNorth(command);
+        } else if (E.equals(this)) {
+            return handleFaceEast(command);
+        } else if (S.equals(this)) {
+            return handleFaceSouth(command);
+        } else {
+            return handleFaceWest(command);
+        }
+    }
+
+    private Direction handleFaceSouth(String command) {
+        if (isRightCommand(command))
+            return W;
+        else
+            return E;
+    }
+
+    private Direction handleFaceEast(String command) {
+        if (isRightCommand(command))
+            return S;
+        else
+            return N;
+    }
+
+    private Direction handleFaceNorth(String command) {
+        if (isRightCommand(command))
+            return E;
+        else
+            return W;
+    }
+
+    private Direction handleFaceWest(String command) {
+        if (isRightCommand(command))
+            return N;
+        else
+            return S;
+    }
+
+    private boolean isRightCommand(String command) {
+        return "R".equalsIgnoreCase(command);
     }
 }
 
